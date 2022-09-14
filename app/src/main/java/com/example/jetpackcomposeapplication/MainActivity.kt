5dd651dedeb8f8ed42dd9577ff71ab36babce0f1
tkmarvin <marvin@asilimia.co.ke>
2022-09-14 17:52:31 +0300

    refactor: adopt good code practice

--- app/src/main/java/com/example/jetpackcomposeapplication/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeapplication/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -76,6 +77,8 @@ showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     JetpackComposeApplicationTheme {
-        BirthdayGreetingWithImage(message = "Happy Birthday Mjay","- from Marvin")
+        BirthdayGreetingWithImage(message = stringResource(R.string.happy_birthday),
+                    stringResource(R.string.signature_text)
+                )
     }
 }
