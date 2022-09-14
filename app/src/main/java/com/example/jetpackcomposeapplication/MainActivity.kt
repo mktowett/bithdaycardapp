b4727f51b3a2dee479da83147505ff802a539afa
tkmarvin <marvin@asilimia.co.ke>
2022-09-14 17:37:02 +0300

    position and scale the Image composable

--- app/src/main/java/com/example/jetpackcomposeapplication/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeapplication/MainActivity.kt
@@ -4,15 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
@@ -50,7 +48,12 @@ fun BirthdayGreetingWithText(message: String,from: String){
 fun BirthdayGreetingWithImage(message: String, from: String) {
     val image = painterResource(id = R.drawable.androidparty)
     Box {
-        Image(painter = image, contentDescription = null)
+        Image(painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(),
+        contentScale = ContentScale.Crop)
         BirthdayGreetingWithText(message = message, from = from)
     }
 
